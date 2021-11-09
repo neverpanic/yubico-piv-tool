662c4f33355329e62fbefdd0aaa18ea1c0492f32
Per Nilsson <per.nilsson@yubico.com>
2021-11-09 15:05:26 +0100

    Increase to safe size

--- lib/internal.h
+++ lib/internal.h
@@ -154,7 +154,7 @@ typedef struct _ykpiv_version_t {
 struct ykpiv_state {
   SCARDCONTEXT context;
   SCARDHANDLE card;
-  char reader[1024];
+  char reader[2048];
   int  verbose;
   int tries;
   char *pin;
